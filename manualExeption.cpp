165bb2e1a97412913afb71fccc4da2b57bcdb2c3
nauvalachid <nauvalachid@gmail.com>
2025-06-10 18:44:29 +0700

    Menambahkan implementasi blok try-catch untuk penanganan exception dalam fungsi main

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; // melemparkan sebuah integer maka
+        cout << "Pernyataan ini tidak akan dieksekusi" << endl;
+    }
+    catch (int a){
+        // blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+}
