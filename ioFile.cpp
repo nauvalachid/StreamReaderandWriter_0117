5dfef664b8a5bca12b3d4286a73ad32e084547ea
nauvalachid <nauvalachid@gmail.com>
2025-06-10 17:29:25 +0700

    Menambahkan implementasi dasar untuk menulis ke file

--- ioFile.cpp
+++ ioFile.cpp
@@ -2,3 +2,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    string baris;
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file.
+    outfile.open("contohfile.txt");
+    cout << ">= Menulis file. \'q\' untuk keluar." << endl;
+    // unlimited loop untuk menulis
+}
