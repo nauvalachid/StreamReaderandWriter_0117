cc2de81d4a6626beac947d17a7a8f4a585df9dc4
nauvalachid <nauvalachid@gmail.com>
2025-06-10 18:09:47 +0700

    Menambahkan implementasi fungsi main untuk membuka file dalam mode menulis

--- ioFIleWithPath.cpp
+++ ioFIleWithPath.cpp
@@ -2,3 +2,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    string baris;
+    string NamaFile;
+    cout << "Masukkan Nama File: ";
+    cin >> NamaFile;
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file.
+    outfile.open(NamaFile + ".txt", ios::out);
+}
