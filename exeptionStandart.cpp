cd43b045d59c96040a44d70024032f0d67e888ea
nauvalachid <nauvalachid@gmail.com>
2025-06-10 18:33:22 +0700

    Menambahkan implementasi blok try-catch untuk penanganan exception pada akses elemen array

--- exeptionStandart.cpp
+++ exeptionStandart.cpp
@@ -4,3 +4,14 @@
 #include <array>
 // untuk obyek array yang akan kita gunakan
 using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int,3> data = {10, 20, 30};
+        // Pesan array integer 3 elemen
+        cout <<data.at(5) << endl;
+        // memanggil array elemen ke-5
+    }
+}
